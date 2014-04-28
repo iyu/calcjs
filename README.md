6823b0c0933fba01a1e89c6878715a7fcf81f517
iYU <aihara_yuhei@cyberagent.co.jp>
2014-04-28 22:03:38 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,77 @@
+CalcJs
+==========
+
+Auxiliary arithmetic library. 
+More accurately, and more quickly.
+
+```
+> 0.55 * 100
+55.00000000000001
+> calcjs.multi(0.55, 100)
+55
+
+> 0.1 + 0.1 + 0.1
+0.30000000000000004
+> calcjs.add(0.1, 0.1, 0.1)
+0.3
+
+> 0.00000000123 * 100
+1.23e-7
+> calcjs.multi(0.00000000123, 100)
+1.23e-7
+
+> 0.1 + 0.2 * 0.1
+0.12000000000000001
+> calcjs.begin(0.1).add(0.2).multi(0.1).end()
+0.12
+```
+
+## Installation
+In browsers:
+```
+<script src="calcjs.js"></script>
+```
+
+Using `npm`:
+```
+npm install git+https://github/yuhei-a/calcjs.git@0.0.1
+```
+
+## Usage
+In browsers:
+```
+var calcjs = windows.calcjs;
+```
+
+In Node.js:
+```
+var calcjs = require('calcjs');
+```
+
+```
+// Addition
+// 0.1 + 0.1 + 0.1
+calcjs.add(0.1, 0.1, 0.1)
+// return 0.3
+
+// Subtraction
+// 0.3 - 0.1 - 0.1
+calcjs.sub(0.3, 0.1, 0.1)
+// return 0.1
+
+// Multiplication
+// 0.1 * 0.1 * 0.1
+calcjs.multi(0.1, 0.1, 0.1)
+// return 0.001
+
+// Division
+// 10 / 5 / 2
+calcjs.div(10, 5, 2)
+// return 1
+
+
+// Chain
+// 0.1 + 0.2 * 0.1
+calcjs.begin(0.1).add(0.2).multi(0.1).end()
+// return 0.12
+```
---
+++ calcjs.js
@@ -0,0 +1,155 @@
+/**
+ * CalcJs
+ * @name calcjs.js
+ * @author Yuhei Aihara <aihara_yuhei2cyberagent.co.jp>
+ * @license MIT License
+ */
+(function() {
+
+  var root = this;
+
+  var previousCalcJs = root.calcjs;
+  
+  var version = '0.0.1';
+  
+  function digit(list) {
+    var result = 0;
+    for (var i = 0; i < list.length; i++) {
+      var str = list[i].toString();
+      var index = str.indexOf('.');
+      var ret = ~index ? str.length - index - 1 : 0;
+      if (result < ret) {
+        result = ret;
+      }
+    }
+  
+    return result;
+  }
+  
+  function _CalcJs(num) {
+    this.nums = [num];
+    this.operators = [];
+  }
+  
+  _CalcJs.prototype.add = function(num) {
+    this.operators.push('add');
+    this.nums.push(num);
+    return this;
+  };
+  
+  _CalcJs.prototype.sub = function(num) {
+    this.operators.push('sub');
+    this.nums.push(num);
+    return this;
+  };
+  
+  _CalcJs.prototype.multi = function(num) {
+    this.operators.push('multi');
+    this.nums.push(num);
+    return this;
+  };
+  
+  _CalcJs.prototype.div = function(num) {
+    this.operators.push('div');
+    this.nums.push(num);
+    return this;
+  };
+  
+  _CalcJs.prototype.end = function() {
+    var _digit = Math.pow(10, digit(this.nums));
+  
+    var indexMulti = this.operators.indexOf('multi');
+    var indexDiv = this.operators.indexOf('div');
+    while(~indexMulti || ~indexDiv) {
+      var nextIndex, ret;
+      if (~indexMulti && (!~indexDiv || indexMulti < indexDiv)) {
+        nextIndex = indexMulti + 1;
+        ret = (this.nums[indexMulti] * _digit) * (this.nums[nextIndex] * _digit) / (_digit * _digit);
+        this.nums.splice(indexMulti, 2, ret);
+        this.operators.splice(indexMulti, 1);
+      } else if (~indexDiv) {
+        nextIndex = indexDiv + 1;
+        ret = this.nums[indexDiv] / this.nums[nextIndex];
+        this.nums.splice(indexDiv, 2, ret);
+        this.operators.splice(indexDiv, 1);
+      }
+  
+      indexMulti = this.operators.indexOf('multi');
+      indexDiv = this.operators.indexOf('div');
+    }
+  
+    var result = this.nums[0];
+    for (var i = 0; i < this.operators.length; i++) {
+      if (this.operators[i] === 'add') {
+        result = (result * _digit + this.nums[i + 1] * _digit) / _digit;
+      } else {
+        result = (result * _digit - this.nums[i + 1] * _digit) / _digit;
+      }
+    }
+  
+    this.nums = [];
+    this.operators = [];
+    return result;
+  };
+  
+  
+  function CalcJs() {
+    this.version = version;
+  }
+  
+  CalcJs.prototype.add = function() {
+    var _digit = Math.pow(10, digit(arguments));
+    var result = 0;
+    for (var i = 0; i < arguments.length; i++) {
+      result += arguments[i] * _digit;
+    }
+    return result / _digit;
+  };
+  
+  CalcJs.prototype.sub = function() {
+    var _digit = Math.pow(10, digit(arguments));
+    var result = 0;
+    for (var i = 0; i < arguments.length; i++) {
+      result -= arguments[i] * _digit;
+    }
+    return result / _digit;
+  };
+  
+  CalcJs.prototype.multi = function() {
+    var _digit = Math.pow(10, digit(arguments));
+    var result = 1;
+    for (var i = 0; i < arguments.length; i++) {
+      result *= arguments[i] * _digit;
+    }
+    return result / Math.pow(_digit, arguments.length);
+  };
+  
+  CalcJs.prototype.div = function() {
+    var result = arguments[0];
+    for (var i = 1; i < arguments.length; i++) {
+      result /= arguments[i];
+    }
+    return result;
+  };
+  
+  CalcJs.prototype.begin = function(num) {
+    return new _CalcJs(num);
+  };
+  
+  CalcJs.prototype.noConflict = function() {
+    root.calcjs = previousCalcJs;
+    return this;
+  };
+
+  var calcjs = new CalcJs();
+  if (typeof exports !== 'undefined') {
+    if (typeof module !== 'undefined' && module.exports) {
+      exports = module.exports = calcjs;
+    } else {
+      exports = calcjs;
+    }
+  } else {
+    root.calcjs = calcjs;
+  }
+}.call(this));
+
